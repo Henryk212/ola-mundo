7ae0bee8f846cb97bc36aaa0197036805b9abdc1
Henryk212 <111775517+Henryk212@users.noreply.github.com>
2023-06-05 15:39:55 -0300

    aula 01 alura, totas

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import  {BrowserRouter, Route, Routes}  from 'react-router-dom';
+import Inicio from './paginas/Inicio';
+import SobreMim from './paginas/SobreMim';
+
+
 
 export default function App() {
   return (
-<h1>Olá Mundo</h1>
+    <div>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Inicio/>} />
+        <Route path="/sobremim" element={<SobreMim/>} />
+        <Route path="*" element={<div>Página não encontrada </div>} />
+      </Routes>
+    </BrowserRouter>
+    </div>     
+   
+  );
   
-  )
+
 }
 
